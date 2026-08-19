921149db7b28cadf87e3301faa4112b1c1512d91
j-alecx <jean.alecx@gmail.com>
2026-08-19 11:25:52 +0800

    added gradient component from reactbits

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
 import {Routes, Route} from 'react-router-dom';
-import {ThemeProvider} from './context/ThemeContext';
+import {ThemeProvider, useTheme} from './context/ThemeContext';
 import {TransactionsProvider} from './context/TransactionsContext';
 import AppNavbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
+import GradientWaves from './components/Gradient';
 import Dashboard from './pages/Dashboard';
 import AddTransaction from './pages/AddTransaction';
 import TransactionDetail from './pages/TransactionDetail';
 import Summary from './pages/Summary';
 
+function AppBackground() {
+    const {theme} = useTheme();
+
+    const colors =  theme === 'dark'
+        ? {horizonColor: '#1a1a1a', waveColor: '#8f2557', crestColor: '#e2668f'}
+        : {horizonColor: '#ffffff', waveColor: '#f9adc8', crestColor: '#e2668f'};
+
+        return (
+            <GradientWaves
+                horizonColor={colors.horizonColor}
+                waveColor={colors.waveColor}
+                crestColor={colors.crestColor}
+                speed={1}/>
+        );
+}
+
 export default function App() {
     return (
         <ThemeProvider>
             <TransactionsProvider>
+                <AppBackground />
                 <AppNavbar />
                 <div className="app-body">
                     <Sidebar />
